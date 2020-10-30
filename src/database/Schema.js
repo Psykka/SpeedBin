1941834dc9b6d8264ad7da9dbad946f2a8959d04
Psykka <36780964+Psykka@users.noreply.github.com>
2020-10-30 17:40:02 -0300

    Update Schema.js

--- src/database/Schema.js
+++ src/database/Schema.js
@@ -2,8 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const document = new Schema({
   _id: String,
-  code: { type: String, required: true },
-  createAt: { type: Date, default: Date, index: { expires: '48h' }}
+  code: { type: String, required: true }
 }, { versionKey: false })
 
 module.exports = model('Codes', document)
